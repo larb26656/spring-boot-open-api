e276905cb6a577adebc9b2dde035a9285dcff340
Errortime <larb26656@gmail.com>
2025-04-07 00:03:13 +0700

    feat: implement demo of redoc and scalar

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+//	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.6")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
